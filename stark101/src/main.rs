a9cbd8983ccd9f1f7b34f414f0a593c7d7dffda4
Hamza Jeljeli <hamzajeljeli@live.fr>
2025-03-13 21:44:05 +0900

    finish part3

--- stark101/src/main.rs
+++ stark101/src/main.rs
@@ -7,5 +7,7 @@ fn main() {
     println!("proof: {:?}", channel.proof);
     let (CP, CP_eval, CP_merkle) = part2::run_part2(g, &eval_domain, f, &mut channel);
     println!("proof: {:?}", channel.proof);
-    let _ = part3::run_part3(eval_domain, CP, CP_eval, CP_merkle);
+    let (fri_polys, fri_domains, fri_layers, fri_merkles) =
+        part3::run_part3(eval_domain, CP, CP_eval, CP_merkle, &mut channel);
+    println!("proof: {:?}", channel.proof);
 }
--- stark101/src/part3.rs
+++ stark101/src/part3.rs
@@ -1,13 +1,19 @@
 use ark_ff::Field;
-use ark_poly::univariate::DensePolynomial;
+use ark_poly::{univariate::DensePolynomial, Polynomial};
 use rs_merkle::{algorithms::Sha256, MerkleTree};
-use stark101::finite_fields::MyField;
+use stark101::{channel::Channel, finite_fields::MyField, fri::generate_fri_commitments};
 
 pub fn run_part3(
     eval_domain: Vec<MyField>,
     CP: DensePolynomial<MyField>,
     CP_eval: Vec<MyField>,
     CP_merkle: MerkleTree<Sha256>,
+    channel: &mut Channel,
+) -> (
+    Vec<DensePolynomial<MyField>>,
+    Vec<Vec<MyField>>,
+    Vec<Vec<MyField>>,
+    Vec<MerkleTree<Sha256>>,
 ) {
     println!("Executing part 3...");
     // FRI folding
@@ -17,4 +23,27 @@ pub fn run_part3(
         eval_domain[100].pow(&(vec![2])),
         eval_domain[half_domain_size + 100].pow(&(vec![2]))
     );
+    // Generate FRI commitments
+    let (fri_polys, fri_domains, fri_layers, fri_merkles) =
+        generate_fri_commitments(&CP, &eval_domain, &CP_eval, &CP_merkle, channel);
+    assert_eq!(fri_layers.len(), 11, "Expected number of FRI layers is 11");
+    assert_eq!(
+        fri_layers.last().unwrap().len(),
+        8,
+        "Expected last layer to contain exactly 8 elements"
+    );
+    assert!(
+        fri_layers
+            .last()
+            .unwrap()
+            .iter()
+            .all(|&x| x == MyField::from(1150958405)),
+        "Expected last layer to be constant"
+    );
+    assert_eq!(
+        fri_polys.last().unwrap().degree(),
+        0,
+        "Expacted last polynomial to be constant (degree 0)"
+    );
+    (fri_polys, fri_domains, fri_layers, fri_merkles)
 }
